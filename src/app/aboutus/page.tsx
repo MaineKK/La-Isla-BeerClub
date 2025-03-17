5a46c4b654a33a2b4bec8e316b7e96d957163362
MaineKK <maralinew@gmail.com>
2025-03-17 16:45:45 +0100

    Retoques fotos

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -9,12 +9,16 @@ const horizontalImages = [
   { src: "/images/jefesss.png" },
   { src: "/images/barra.png" },
   { src: "/images/artesanas3.png" },
+  { src: "/images/delirium.jpeg" },
+  { src: "/images/local.jpeg" },
 ];
 
 const verticalImages = [
   { src: "/images/terraza.png" },
   { src: "/images/artesanas.jpg" },
-  { src: "/images/internacional.jpg" },
+  { src: "/images/logo.jpeg" },
+  { src: "/images/chouffe.jpeg" },
+  { src: "/images/exterior.jpeg" },
 ];
 
 export default function AboutUs() {
@@ -22,10 +26,10 @@ export default function AboutUs() {
     <div className="min-h-screen w-full flex flex-col items-center bg-[#d1af95]">
       <NavBar />
 
-      <div className="relative w-full max-w-5xl mt-18 px-10 py-28 text-center bg-[#b59b83] rounded-xl shadow-lg overflow-hidden">
+      <div className="relative w-full max-w-5xl mt-18 px-8 py-22 text-center bg-[#b59b83] rounded-xl shadow-lg overflow-hidden">
         <div className="absolute inset-0 w-full h-full">
           <Image
-            src="/images/fondobg.png"
+            src="/images/fondonosotros.png"
             alt="Fondo decorativo"
             layout="fill"
             objectFit="cover"
@@ -34,10 +38,10 @@ export default function AboutUs() {
         </div>
 
         {/* Texto de la Cabecera */}
-        <h1 className="relative text-2xl font-bold text-[#e0bea9] mt-14 z-10">
+        <h1 className="relative text-2xl font-bold text-[#e0bea9] mt-16 z-10">
           Un murciano y dos canarios entran en un bar…
         </h1>
-        <p className="relative text-l max-w-4xl mx-auto text-[#d4b9aa] z-10 mt-10 mb-32">
+        <p className="relative text-sm max-w-3xl mx-auto text-[#d4b9aa] z-10 mt-2 mb-32">
           Podría ser un chiste, pero el resultado fue una de las cervecerías más relevantes en Murcia.
           <br />
           Tenían un objetivo claro: crear un espacio hogareño y divertido donde no parase de correr la buena cerveza.
@@ -55,7 +59,7 @@ export default function AboutUs() {
             <Carousel
               autoPlay
               infiniteLoop
-              interval={4000}
+              interval={3000}
               showThumbs={false}
               showStatus={false}
               dynamicHeight={false}
@@ -85,22 +89,18 @@ export default function AboutUs() {
         </div>
 
         
-        <div className="mt-24"></div>
+        <div className="mt-20"></div>
 
         {/* Segundo bloque */}
         <div className="relative flex flex-col md:flex-row items-center md:items-start max-w-4xl mx-auto z-10 gap-20">
          
-          <p className="text-base text-[#5C3B28] md:w-1/2 text-justify leading-relaxed">
-            Todo esto sería imposible sin nuestro equipo, una gran familia de profesionales cualificados y cercanos
-            siempre listos para generar el mejor ambiente y demostrar lo que fielmente creemos, que hay una cerveza
-            para cada persona.
-          </p>
+     
 
           <div className="w-full md:w-1/2 flex justify-center">
             <Carousel
               autoPlay
               infiniteLoop
-              interval={4000}
+              interval={2000}
               showThumbs={false}
               showStatus={false}
               dynamicHeight={false}
@@ -119,6 +119,11 @@ export default function AboutUs() {
               ))}
             </Carousel>
           </div>
+          <p className="text-base text-[#5C3B28] md:w-1/2 text-justify leading-relaxed">
+            Todo esto sería imposible sin nuestro equipo, una gran familia de profesionales cualificados y cercanos
+            siempre listos para generar el mejor ambiente y demostrar lo que fielmente creemos, que hay una cerveza
+            para cada persona.
+          </p>
         </div>
       </div>
     </div>
