a9e968b0c70e8d859493aea82c3ed3f76cdbf875
MaineKK <maralinew@gmail.com>
2025-06-05 01:38:27 +0200

    retouch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Navbar from "./NavBar";
 import MenuSection from "./MenuSection";
-import Head from "next/head";
+
 
 export default function Home() {
   return (
     <>
-    <Head>
-        <title>La Isla Beer Club</title>
-      </Head>
+
 
     <main className="min-h-screen bg-[#d1af95] flex flex-col items-center pb-16">
       <Navbar />
